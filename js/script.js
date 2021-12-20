0fbffcc1d141345da59bf0142bc863abaac1c1ad
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2021-12-20 14:26:42 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log ('JS OK');
+
+/*
+1. Chiedi all’utente il suo nome
+2. Chiedi il suo cognome
+3. Chiedi il suo colore preferito
+4. Scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+// Chiedi all'utente il suo nome
+
